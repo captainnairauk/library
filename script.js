d6ee0608a1f8da76170cecc4922a9d9d9464b236
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-05-08 17:27:11 +0530

    Created popup modal.

--- script.js
+++ script.js
@@ -17,24 +17,33 @@ function Book(title, author, pages, didYouread) {
     }
 }
 
-function addBookToLibrary() {
-    const title = prompt("Enter the title");
-    const titleString = title.toString();
+// function addBookToLibrary() {
+//     const title = prompt("Enter the title");
+//     const titleString = title.toString();
 
-    const author = prompt("Enter the name of the author");
-    const authorString = author.toString();
+//     const author = prompt("Enter the name of the author");
+//     const authorString = author.toString();
 
-    const pages = prompt("Enter the number of pages");
-    const pagesString = pages.toString();
+//     const pages = prompt("Enter the number of pages");
+//     const pagesString = pages.toString();
 
-    const didYouread = prompt("Answer in true or false");
-    const didYoureadString = didYouread.toLowerCase();
+//     const didYouread = prompt("Answer in true or false");
+//     const didYoureadString = didYouread.toLowerCase();
 
-    const book = new Book(titleString, authorString, pagesString, didYoureadString);
-    console.log(book.info());
-    myLibrary.push(book);
-    console.log(myLibrary);
-    addBookToLibrary();
-}
+//     const book = new Book(titleString, authorString, pagesString, didYoureadString);
+//     console.log(book.info());
+//     myLibrary.push(book);
+//     console.log(myLibrary);
+//     addBookToLibrary();
+// }
 
-addBookToLibrary();
+// addBookToLibrary();
+
+document.querySelector("#addBook").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+
