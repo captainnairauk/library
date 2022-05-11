99ba1b177ba480c6a70d010bac3e19521cc24b36
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-05-12 01:13:13 +0530

    Added functional table.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+let selectedRow = null
 
 function Book(title, author, pages, didYouread) {
     this.title = title;
@@ -15,40 +15,17 @@ function Book(title, author, pages, didYouread) {
     }
 }
 
-// function addBookToLibrary() {
-//     const title = prompt("Enter the title");
-//     const titleString = title.toString();
 
-//     const author = prompt("Enter the name of the author");
-//     const authorString = author.toString();
-
-//     const pages = prompt("Enter the number of pages");
-//     const pagesString = pages.toString();
-
-//     const didYouread = prompt("Answer in true or false");
-//     const didYoureadString = didYouread.toLowerCase();
-
-//     const book = new Book(titleString, authorString, pagesString, didYoureadString);
-//     console.log(book.info());
-//     myLibrary.push(book);
-//     console.log(myLibrary);
-//     addBookToLibrary();
-// }
-
-// addBookToLibrary();
-
-document.querySelector("#addBook").addEventListener("click",function(){
+document.querySelector("#addBook").addEventListener("click", function () {
     document.querySelector(".popup").classList.add("active");
 });
 
-document.querySelector(".popup .close-btn").addEventListener("click",function(){
+document.querySelector(".popup .close-btn").addEventListener("click", function () {
     document.querySelector(".popup").classList.remove("active");
 });
 
 
-
-const addBookToLibrary = (event) =>{
-    event.preventDefault();
+const addBookToLibrary = () => {
 
     let book = {
         title: document.getElementById("title").value,
@@ -57,17 +34,55 @@ const addBookToLibrary = (event) =>{
         didYouread: document.getElementById("readOrNot").checked
     }
 
-    const libBooks = new Book(book["title"],book["author"],book["pages"],book["didYouread"]);
+    const libBooks = new Book(book["title"], book["author"], book["pages"], book["didYouread"]); 
     console.log(libBooks.info());
     console.log(document.getElementById("readOrNot").value);
-    myLibrary.push(libBooks);
     document.forms[0].reset();
-    
+    selectedRow = null;
 
-    console.warn("added", {myLibrary});
-    createTable();
+    if (selectedRow == null){
+        insertNewRecord(libBooks);
+
+    } else{
+        updateRecord(libBooks)
+    }
+
 }
 
-document.addEventListener("DOMContentLoaded", () =>{
-    document.getElementById("submitBtn").addEventListener("click",addBookToLibrary);
-});
+function insertNewRecord(data){
+    let table = document.getElementById("bookList").getElementsByTagName("tbody")[0];
+    let newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.title;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.author;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.pages;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.didYouread;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick= "onEdit(this)" href="#">Edit</a>
+                       <a onClick= "onDelete(this)" href="#">Delete</a>`;
+}
+
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("title").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("author").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("pages").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("readOrNot").value = selectedRow.cells[3].innerHTML;
+}
+
+function onDelete(td){
+    row = td.parentElement.parentElement;
+    document.getElementById("bookList").deleteRow(row.rowIndex);
+    document.forms[0].reset();
+}
+
+function updateRecord(formData){
+    selectedRow.cells[0].innerHTML = formData.title;
+    selectedRow.cells[1].innerHTML = formData.author;
+    selectedRow.cells[2].innerHTML = formData.pages;
+    selectedRow.cells[3].innerHTML = formData.didYouread;
+    
+}
